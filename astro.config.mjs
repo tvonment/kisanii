e4b057712aac26651a8d826863117a6ad9fd4a61
tvonment <thomas@kisanii.ch>
2026-06-06 21:08:54 +0200

    feat: activate custom domain www.kisanii.ch

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://tvonment.github.io',
-  base: '/kisanii',
+  site: 'https://www.kisanii.ch',
+  base: '/',
   integrations: [
     sitemap({
       filter: (page) => {
